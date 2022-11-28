8bff7a26f7631db714bf2ec97cb5423b92fa82d8
abdelhay-el <abdelhay.elouardi@usmba.ac.ma>
2022-11-28 17:20:16 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿using System;
+
+namespace FormApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnID = new System.Windows.Forms.Label();
+            this.btnName = new System.Windows.Forms.Label();
+            this.textID = new System.Windows.Forms.TextBox();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.cmbBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(90, 375);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(221, 375);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(75, 23);
+            this.btnSelect.TabIndex = 1;
+            this.btnSelect.Text = "Select";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(352, 375);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 2;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(471, 375);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(608, 375);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 4;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnID
+            // 
+            this.btnID.AutoSize = true;
+            this.btnID.Location = new System.Drawing.Point(131, 204);
+            this.btnID.Name = "btnID";
+            this.btnID.Size = new System.Drawing.Size(20, 16);
+            this.btnID.TabIndex = 5;
+            this.btnID.Text = "ID";
+            this.btnID.Click += new System.EventHandler(this.btnID_Click);
+            // 
+            // btnName
+            // 
+            this.btnName.AutoSize = true;
+            this.btnName.Location = new System.Drawing.Point(131, 296);
+            this.btnName.Name = "btnName";
+            this.btnName.Size = new System.Drawing.Size(44, 16);
+            this.btnName.TabIndex = 6;
+            this.btnName.Text = "Name";
+            this.btnName.Click += new System.EventHandler(this.btnName_Click);
+            // 
+            // textID
+            // 
+            this.textID.Location = new System.Drawing.Point(249, 204);
+            this.textID.Name = "textID";
+            this.textID.Size = new System.Drawing.Size(161, 22);
+            this.textID.TabIndex = 7;
+            this.textID.TextChanged += new System.EventHandler(this.textID_TextChanged);
+            // 
+            // textName
+            // 
+            this.textName.Location = new System.Drawing.Point(249, 293);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(161, 22);
+            this.textName.TabIndex = 8;
+            this.textName.TextChanged += new System.EventHandler(this.textName_TextChanged);
+            // 
+            // cmbBox
+            // 
+            this.cmbBox.FormattingEnabled = true;
+            this.cmbBox.Location = new System.Drawing.Point(168, 75);
+            this.cmbBox.Name = "cmbBox";
+            this.cmbBox.Size = new System.Drawing.Size(449, 24);
+            this.cmbBox.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmbBox);
+            this.Controls.Add(this.textName);
+            this.Controls.Add(this.textID);
+            this.Controls.Add(this.btnName);
+            this.Controls.Add(this.btnID);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e) => throw new NotImplementedException();
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label btnID;
+        private System.Windows.Forms.Label btnName;
+        private System.Windows.Forms.TextBox textID;
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.ComboBox cmbBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace FormApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static string con = @"Data Source=ABDE\SQLEXPRESS;Initial Catalog=ConnectionDB;Integrated Security=True";
+        static SqlConnection cnx = new SqlConnection(con);
+        static SqlCommand cmd = new SqlCommand();
+        static SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+        public void show()
+        {
+            cmbBox.Items.Clear();
+            cmd.Connection = cnx;
+            cmd.CommandText = "select * from FORM";
+            if (cmd.Connection.State == ConnectionState.Open)
+            {
+                cmd.Connection.Close();
+            }
+            cnx.Open();
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+
+            foreach (DataRow dr in dt.Rows)
+            {
+
+                cmbBox.Items.Add("Name : " + dr["name"]);
+                cmbBox.Items.Add("Id   : " + dr["id"].ToString());
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnAdd.Enabled = true;
+            btnSelect.Enabled = false;
+            btnEdit.Enabled = false;
+            btnDelete.Enabled = true;
+            btnCancel.Enabled = true;
+            cmbBox.Enabled = true;
+            textID.Enabled = false;
+            textName.Enabled = false;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnID_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textID_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnName_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void textName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender , EventArgs e)
+        {
+            this.Text = "Add";
+            textID.Clear();
+            textName.Clear();
+            show();
+            btnAdd.Enabled = false;
+            btnSelect.Enabled = true;
+            btnEdit.Enabled = false;
+            btnDelete.Enabled = true;
+            btnCancel.Enabled = false;
+            cmbBox.Enabled = false;
+            textID.Enabled = true;
+            textName.Enabled = true;
+        }
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            this.Text = "Edit";
+            btnAdd.Enabled = false;
+            btnSelect.Enabled = true;
+            btnEdit.Enabled = false;
+            btnDelete.Enabled = false;
+            btnCancel.Enabled = true;
+            cmbBox.Enabled = true;
+            textID.Enabled = true;
+            textName.Enabled = true;
+
+           
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
